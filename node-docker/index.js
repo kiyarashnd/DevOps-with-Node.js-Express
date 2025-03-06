db002a3aadb60049a3d492776870848cf0c5af2d
kiyarash <kiarashnadri@yahoo.com>
2025-03-06 15:06:29 +0330

    problem with network

--- node-docker/index.js
+++ node-docker/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose
+  .connect('mongodb://sanjeev:mypassword@172.19.0.2:27017/?authSource=admin')
+  .then(() => console.log('succesfully connected to DB'))
+  .catch((e) => console.log(e));
+
 app.get('/', (req, res) => {
-  res.send('<h2>HI there</h2>');
+  res.send('<h2>HI there!!</h2>');
 });
 
 const port = process.env.PORT || 3000;
